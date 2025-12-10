cedcf9fba010c3e35a8c62d5cc4dc5e9772f9a81
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-10 23:22:03 +0530

    Maximum Subarray Sum.py

---
+++ Array/Maximum Subarray Sum.py
@@ -0,0 +1,9 @@
+def maxSubArray(nums):
+    best = curr = nums[0]
+    for x in nums[1:]:
+        curr = max(x, curr + x)
+        best = max(best, curr)
+    return best
+
+
+print(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
