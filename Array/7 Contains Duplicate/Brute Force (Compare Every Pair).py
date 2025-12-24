4b62bee2b6c7c514cf37cd5c92be440997cad623
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-24 14:57:46 +0530

    Brute Force (Compare Every Pair) Done Contains Duplicate

---
+++ Array/7 Contains Duplicate/Brute Force (Compare Every Pair).py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+
+# ⏱ Time Complexity
+# O(n²) ❌
+
+# 📦 Space Complexity
+# O(1)
