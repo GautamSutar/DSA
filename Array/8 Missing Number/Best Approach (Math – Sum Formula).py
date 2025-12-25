653db36a010751d4f799f043e4047e1fdaa806d7
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-25 17:43:11 +0530

    Best Approach (Math – Sum Formula) Missing Number

---
+++ Array/8 Missing Number/Best Approach (Math – Sum Formula).py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected = n * (n + 1) // 2
+        actual = sum(nums)
+        return expected - actual
+
+
+
+# ⏱ Time Complexity
+# O(n)
+
+# 📦 Space Complexity
+# O(1)
