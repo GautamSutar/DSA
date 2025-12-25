f2676b3e76df40240407f5838e96b752aa648f4d
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-25 18:58:01 +0530

    Better Approach (Hash Set) Find All Numbers Disappeared in an Array

---
+++ Array/9 Find All Numbers Disappeared in an Array/Better Approach (Hash Set).py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        seen = set(nums)
+        result = []
+
+        for i in range(1, len(nums) + 1):
+            if i not in seen:
+                result.append(i)
+
+        return result
+
+# ⏱ Time Complexity
+# O(n)
+
+# 📦 Space Complexity
+# O(n) ❌
+
