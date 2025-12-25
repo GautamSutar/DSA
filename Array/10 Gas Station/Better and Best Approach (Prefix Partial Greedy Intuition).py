84b0bd44640f855454fbe104af82e19b272aeec4
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-25 22:08:11 +0530

    Better and Best Approach (Prefix Partial Greedy Intuition).py

---
+++ Array/10 Gas Station/Better and Best Approach (Prefix Partial Greedy Intuition).py
@@ -0,0 +1,23 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total = 0
+        curr = 0
+        start = 0
+
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            total += diff
+            curr += diff
+
+            if curr < 0:
+                start = i + 1
+                curr = 0
+
+        return start if total >= 0 else -1
+
+
+# | Approach          | Time     | Space    | Status |
+# | ----------------- | -------- | -------- | ------ |
+# | Brute             | O(n²)    | O(1)     | ❌      |
+# | Better            | O(n)     | O(1)     | ✅      |
+# | **Best (Greedy)** | **O(n)** | **O(1)** | ⭐      |
