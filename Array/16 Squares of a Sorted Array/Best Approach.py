c1369172a3c04dc3e8508667190dec30ba869d0c
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-27 16:12:04 +0530

    Best Approach Done

---
+++ Array/16 Squares of a Sorted Array/Best Approach.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+
+        left = 0
+        right = n - 1
+        pos = n - 1
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                res[pos] = nums[left] * nums[left]
+                left += 1
+            else:
+                res[pos] = nums[right] * nums[right]
+                right -= 1
+            pos -= 1
+
+        return res
+
+
+# ⏱ Time Complexity
+# O(n) (single pass)
+
+# 🧠 Space Complexity
+# O(n) (output array)
