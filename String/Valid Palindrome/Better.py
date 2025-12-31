c4f6b6eba25a9f62387912d5f7bf1aff3765280c
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-31 17:49:35 +0530

    Better Done - Palindrome

---
+++ String/Valid Palindrome/Better.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = []
+
+        for ch in s:
+            if ch.isalnum():
+                cleaned.append(ch.lower())
+
+        l, r = 0, len(cleaned) - 1
+        while l < r:
+            if cleaned[l] != cleaned[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+
+# Complexity
+
+# Time: O(n)
+
+# Space: O(n)
