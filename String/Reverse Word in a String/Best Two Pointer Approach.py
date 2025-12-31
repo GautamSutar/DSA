dbf6b709a0e4c1b5a55b7d478ba0168e7b2bca91
Gautam Sutar <gautamsutar.in@gmail.com>
2025-12-31 16:34:38 +0530

    Best Reverse Word In A String

---
+++ String/Reverse Word in a String/Best Two Pointer Approach.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        n = len(s)
+        i = n - 1
+        result = []
+
+        while i >= 0:
+            # skip spaces
+            while i >= 0 and s[i] == " ":
+                i -= 1
+            if i < 0:
+                break
+
+            j = i
+            # capture word
+            while i >= 0 and s[i] != " ":
+                i -= 1
+
+            result.append(s[i + 1 : j + 1])
+
+        return " ".join(result)
