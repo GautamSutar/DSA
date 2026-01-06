06cfca2c33cae750aefe1ac61557aba2a94376c8
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-06 19:18:33 +0530

    simpleToLowerCase Done

---
+++ String/To Lower Case/simpleToLowerCase.py
@@ -0,0 +1,10 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s = s.lower()
+        return s
+ob = Solution()
+print(ob.toLowerCase("Hello World!"))
+
+# Time Complexity: `O(n)`
+# Space Complexity: `O(n)`
+
