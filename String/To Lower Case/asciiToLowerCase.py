1863b8cf6803f9e13ad77a99f69c54eb0aa33c8b
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-06 19:35:43 +0530

    asciiToLowerCase Done

---
+++ String/To Lower Case/asciiToLowerCase.py
@@ -0,0 +1,18 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        result = []
+
+        for ch in s:
+            if "A" <= ch <= "Z":
+                result.append(chr(ord(ch) + 32))
+            else:
+                result.append(ch)
+
+        return "".join(result)
+
+
+# Complexity
+
+# Time: O(n)
+
+# Space: O(n)
