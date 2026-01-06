ebfdb444f1d38f571b6f054f6e107c02ec8e4a1c
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-06 18:52:37 +0530

    betterLengthofLastWord Done

---
+++ String/Length of Last Word/betterLengthofLastWord.py
@@ -0,0 +1,17 @@
+def lengthOfLastWord(s: str) -> int:
+    cleaned_text = s.strip()
+    i = len(cleaned_text) - 1
+    print(cleaned_text)
+    print(i)
+    if i == 0:
+        return 1
+    count = 0
+    while i >= 0:
+        if cleaned_text[i] == " ":
+            break
+        count += 1
+        i -= 1
+    return count    
+
+
+print(lengthOfLastWord("    day"))
