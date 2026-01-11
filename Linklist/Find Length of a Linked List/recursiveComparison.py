cfe73a09d84b87347331fd476418a48372d4a856
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-11 18:40:05 +0530

    recursiveComparison

---
+++ Linklist/Find Length of a Linked List/recursiveComparison.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        self.left = head
+
+        def check(right):
+            if not right:
+                return True
+            if not check(right.next):
+                return False
+            is_equal = self.left.val == right.val
+            self.left = self.left.next
+            return is_equal
+
+        return check(head)
