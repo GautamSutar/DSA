a2e0c25879fd8ebccd12b2506fbc9be8ed00ea8a
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-14 21:43:06 +0530

    bestApproachDeleteNode

---
+++ Linklist/3-Delete Node in a Linked List/bestApproachDeleteNode.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+# ⏱ Complexity
+
+# Time: O(1) ⭐
+
+# Space: O(1) ⭐
