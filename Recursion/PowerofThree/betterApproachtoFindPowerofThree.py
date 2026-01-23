74c7b0942f0e564a2055da39f3c37c29316693ca
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-23 15:54:33 +0530

    betterApproachtoFindPowerofThree

---
+++ Recursion/PowerofThree/betterApproachtoFindPowerofThree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n % 3 != 0:
+            return False
+        return self.isPowerOfThree(n // 3)
+
+
+# | Type  | Value                           |
+# | ----- | ------------------------------- |
+# | Time  | **O(log₃ n)**                   |
+# | Space | **O(log₃ n)** (recursion stack) |
