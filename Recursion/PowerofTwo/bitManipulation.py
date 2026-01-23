4196c128c06aa40e92acc3e5e2d957370250bd14
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-23 15:37:29 +0530

    bitManipulation

---
+++ Recursion/PowerofTwo/bitManipulation.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and (n & (n - 1)) == 0
+
+# Complexity
+
+# Time: O(1)
+
+# Space: O(1)
+
+# ✅ This is the expected interview solution
