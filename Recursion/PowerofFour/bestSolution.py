f6f06cc73acc29576fb650025f16c7e664f47596
Gautam Sutar <gautamsutar.in@gmail.com>
2026-01-23 16:04:37 +0530

    bestSolution Updated

--- Recursion/PowerofFour/bestSolution.py
+++ Recursion/PowerofFour/bestSolution.py
@@ -1,3 +1,9 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
+
+
+# | Type  | Value    |
+# | ----- | -------- |
+# | Time  | **O(1)** |
+# | Space | **O(1)** |
