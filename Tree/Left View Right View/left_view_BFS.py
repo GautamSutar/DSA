684c65b47d6e2bc05ac5ed8fdd76d9461d497fd3
Gautam Sutar <gautamsutar.in@gmail.com>
2026-02-15 22:41:48 +0530

    right DFS

--- Tree/Left View Right View/left_view_BFS.py
+++ Tree/Left View Right View/left_view_BFS.py
@@ -1,5 +1,6 @@
+
 class Node:
-    def __init_(self, val):
+    def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
@@ -10,9 +11,19 @@ def left_view(root):
     queue = deque([root])
     result = []
     while queue:
-        node = queue.popleft()
-        result.append(node.val)
-        
+        level_size = len(queue)
+        for i in range(level_size):
+            node = queue.popleft()
+            if i == 0:
+                result.append(node.val)
+            
+            if node.left:
+                queue.append(node.left)
+            
+            if node.right:
+                queue.append(node.right)
+    
+    print(result)
         
 
 
@@ -21,6 +32,6 @@ root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
+root.left.right.left = Node(7)
+root.right.right = Node(6)
 left_view(root)
---
+++ Tree/Left View Right View/right_view_DFS.py
@@ -0,0 +1,33 @@
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+    
+    
+def right_view_dfs(root):
+    result = []
+
+    def helper(node, level):
+        if not node:
+            return
+        
+        if level == len(result):
+            result.append(node.val)
+        
+        helper(node.right, level + 1)
+        helper(node.left, level + 1)
+    
+    helper(root, 0)
+    return result
+    
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.left.right.left = Node(7)
+root.right.right = Node(6)
+answer = right_view_dfs(root)
+print(answer)
