0282037e72670f76434ace5cd478dd517555a73c
Gautam Sutar <gautamsutar.in@gmail.com>
2026-03-31 22:47:58 +0530

    alternate_max_min

---
+++ Infosys/Array/alternate_max_min/alternate_max_min.py
@@ -0,0 +1,12 @@
+def alternate_max_min(arr):
+    count = 0
+    while len(arr) > 1:
+        if count % 2 == 0:
+            n = arr.pop(arr.index(max(arr)))
+        else:
+            n = arr.pop(arr.index(min(arr)))
+        count += 1
+    return arr[0]
+
+arr = list(map(int, input().split()))
+print(alternate_max_min(arr))
