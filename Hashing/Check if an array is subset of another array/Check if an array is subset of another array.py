bea6569423b3b517e903216a23d9dc224f2c9e98
Gautam Sutar <gautamsutar.in@gmail.com>
2026-04-19 21:30:27 +0530

    Check if an array is subset of another array

---
+++ Hashing/Check if an array is subset of another array/Check if an array is subset of another array.py
@@ -0,0 +1,14 @@
+class ToCheckSubset:
+    def __init__(self, array1, array2):
+        self.arr1 = array1
+        self.arr2 = array2
+    def check(self):
+        for num in self.arr2:
+            if num not in self.arr1:
+                return False
+        return True
+
+array1 = [10, 5, 2, 23, 19]
+array2 = [19, 5, 3] 
+obj = ToCheckSubset(array1, array2)
+print(obj.check())
